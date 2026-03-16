f2622c6e8752a5325682ab9ba96f669b4ec75aac
angelinapellat-ux <angelina.pellat@laplateforme.io>
2026-03-16 12:21:42 +0100

    fix : continue

--- interface.py
+++ interface.py
@@ -22,3 +22,24 @@ class RegisterWindow(tk.Toplevel):
         tk.Label(self, text="Email").pack(pady=5)
         self.prenom_var = tk.StringVar()
         tk.Entry(self, textvariable=slef.email_var).pack()
+
+        tk.Label(self, text="Mot de passe").pack(pady=5)
+        self.pwd_var = tk.StringVar()
+        tk.Entry(self, textvariable=self.pwd_var, show="*").pack()
+
+        tk.Label(self, text="Confirmer le mot de passe").pack(pady=5)
+        self.pwd_confirm_var = tk.StringVar()
+        tk.Entry(self, textvariable=self.pwd_confirm_var, show="*").pack()
+
+        tk.Button(self, text="Créer un compte", command=self.register).pack(pady=20)
+
+         def register(self):
+        nom = self.nom_var.get()
+        prenom = self.prenom_var.get()
+        email = self.email_var.get()
+        pwd = self.pwd_var.get()
+        pwd_confirm = self.pwd_confirm_var.get()
+
+        if pwd != pwd_confirm:
+            messagebox.showerror("Erreur", "Les mots de passe ne correspondent pas")
+            return
