f5b127ed5dae3b260b729cdc9b332056623b2e3f
angelinapellat-ux <angelina.pellat@laplateforme.io>
2026-03-16 11:14:37 +0100

    feat : change

--- interface.py
+++ interface.py
@@ -3,12 +3,9 @@ from tkinter import messagebox
 #from (base de donnée) import (ta fonction)
 #from (sécuritée) import (ta fonction)
 
-class LoginWindow(tk.Tk) :
-    def __init__(self) : 
-        super().__init__()
-        self.title("Connexion - Gestion financière")
-
-        tk.Label(self, text="Email").grid(row=0, column=0)
-        tk.Label(self, text="Mot de passe").grid(row=1, column=0)
-
-        
+class RegisterWindow(tk.Toplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        self.title("Inscription")
+        self.geometry("400x350")
+        self.resizable(False, False)
